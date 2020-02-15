a25bf8bb4754634098f7449e9fc58793c9946a93
Paras-code-007 <56780209+Paras-code-007@users.noreply.github.com>
2020-02-15 23:13:01 +0530

    add nice to have msg
    Signed-off-by: Paras-code-007 <56780209+Paras-code-007@users.noreply.github.com>

--- file01.js
+++ file01.js
@@ -2,6 +2,7 @@ function greet(name) {
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
+    console.log('nice to have you here ')
 }
 
 greet('paras')
